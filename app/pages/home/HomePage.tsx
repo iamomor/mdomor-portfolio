fbb0392a1452b858db38f7a9ea0cd7bb8947af1f
Your Name <developeromor@gmail.com>
2026-05-11 06:52:29 +0600

    Add analytics

--- app/pages/home/HomePage.tsx
+++ app/pages/home/HomePage.tsx
@@ -1,19 +1,22 @@
 'use client';
 
-import type { Portfolio } from '../../types/portfolio';
+import { Analytics } from "@vercel/analytics/next";
 import Hero from '../../components/Hero';
 import About from '../../components/about/About';
-import Experience from '../../components/experience/Experience';
+import Blog from '../../components/blog/Blog';
+import Contact from '../../components/contact/Contact';
 import Education from '../../components/education/Education';
+import Experience from '../../components/experience/Experience';
 import Projects from '../../components/projects/Projects';
 import Skills from '../../components/skills/Skills';
-import Blog from '../../components/blog/Blog';
+import type { Portfolio } from '../../types/portfolio';
 // import Beyond from '../../components/beyond/Beyond';
-import Contact from '../../components/contact/Contact';
 
-export default function HomePage({ data }: Readonly<{ data: Portfolio }>) {
+
+export default function HomePage({ data }: Readonly<{ data: Portfolio }>) {   
   return (
     <main>
+      <Analytics />
       <Hero data={data} />
       <About data={data} />
       <Experience data={data} />
